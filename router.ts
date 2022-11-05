3a55381a07aacc070a10983d3dac94306c5b272d
Anny-Oyes <stephanyoyes@gmail.com>
2022-11-04 18:39:34 -0600

    modificando router

--- router.ts
+++ router.ts
@@ -2,11 +2,13 @@ import {Express} from "express";
 import productRouter from "./products/router";
 import authRouter from "./auth/router";
 import storeRouter from "./store/router";
+import invoiceRouter from "./invoce/router";
 
 const router = (app: Express) => {
   app.use("/auth", authRouter);
   app.use("/products", productRouter);
   app.use("/store", storeRouter);
+  app.use("/invoce", invoiceRouter);
 };
 
 export default router;
